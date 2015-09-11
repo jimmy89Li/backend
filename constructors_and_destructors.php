f9033de1bdb3fcee618b4c98417d6254342d7e03
jimmyLi <liviu.c.simion@gmail.com>
2015-09-11 13:03:32 +0200

    Added constructors_and_destructors.php

---
+++ constructors_and_destructors.php
@@ -0,0 +1,13 @@
+<?php
+// Constructors and destructors
+
+class ... {
+	function __construct(){
+		something to do when is constructed;
+	}
+	function __destruct(){
+		something to do when is destructed;
+	}
+}
+
+?>
